532a395ad38cb86686e5f790961501f39eb2a8db
leandroAntunesDosSantos <leandro_antunes28@hotmail.com>
2023-09-14 03:54:33 -0300

    api de pokemon

--- src/controladores/Pokemon.js
+++ src/controladores/Pokemon.js
@@ -12,7 +12,18 @@ const verificarPokemons = async (req, res) => {
 const pegarPokemons = async (req, res) => {
   const { id } = req.params;
   const unico = await detalharPokemon(id);
-  res.status(200).json(unico);
+  const { name, height, weight, base_experience, forms, abilities, species } =
+    unico;
+  return res.json({
+    id,
+    name,
+    height,
+    weight,
+    base_experience,
+    forms,
+    abilities,
+    species,
+  });
 };
 
 module.exports = {
